cc3256bea8f248a2d8d6e389c031ddfa28045754
Rei Imai <rimai1@cs.binghamton.edu>
2019-09-12 17:31:54 -0400

    submitted lab4

---
+++ submit/lab4/ex7/nan.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+  double v;
+  scanf("%lf", &v);
+  double x = v/v;
+  printf("x = %lg\n", x);
+  while (x != x) {
+    //loop
+	
+  }
+  printf("loop exit\n");
+  return 0;
+}
+
+//lf == double
+//lg == long double
---
+++ submit/lab4/ex9/loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+  while (1) {
+    float d;
+    scanf("%f", &d);
+    float x;
+    for (x = d; x <= d + 1000.0; x = x + 1000.0) {
+    }
+    printf("loop exited with x = %.14g\n", x);
+  }
+  return 0;
+}
+
+//2**34 = 17179869184 = 
